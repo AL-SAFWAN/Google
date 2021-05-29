848ff5dcfb057e6af78e7e46aa8184b7f79f5148
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-05-29 15:33:48 +0100

    header component

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+import Avatar from "./Avatar";
+import { ViewGridIcon, MicrophoneIcon } from "@heroicons/react/solid";
 function Header() {
   return (
     <header className=" flex w-full p-5 justify-between text-sm text-gray-700">
@@ -8,6 +10,8 @@ function Header() {
       <div className="flex space-x-4">
         <p className="link">Gmail</p>
         <p className="link">Images</p>
+        <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+        <Avatar url="https://lh3.googleusercontent.com/-9ieZ5JYxRQA/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucky9hy0FxsYfg5ETadAGXDz1vpM4Q/photo.jpg?sz=46" />
       </div>
     </header>
   );
