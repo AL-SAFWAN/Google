c3ada3777a51304dd3e51a3dae8e8605e1480bcd
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-05-30 20:47:58 +0100

    Header Component

---
+++ pages/search.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Header from "../components/Search/Header";
+function Search() {
+  return (
+    <>
+      <Head>
+        <title>Search Result</title>
+      </Head>
+      <Header />
+    </>
+  );
+}
+
+export default Search;
