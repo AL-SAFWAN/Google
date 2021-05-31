5d3b8536fd54ba33959c82b9d9baf404b70f4d86
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-05-31 21:00:50 +0100

    setting up google search engine

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Search/Header";
-function Search() {
+import res from "../res";
+function Search({ result }) {
+  console.log(result);
   return (
     <>
       <Head>
@@ -12,3 +14,20 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = true;
+  const apiKey = process.env.CUSTOM_SEARCH_API;
+  const contextKey = process.env.CONTEXT_KEY;
+  console.log(apiKey, contextKey);
+  const data = useDummyData
+    ? res
+    : await fetch(
+        `https://customsearch.googleapis.com/customsearch/v1?key=${apiKey}&cx=${contextKey}&q=${context.query.term}`
+      ).then((res) => res.json());
+  return {
+    props: {
+      result: data,
+    },
+  };
+}
