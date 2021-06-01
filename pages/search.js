e4361d02513f53d42f78e70dc78fa214cb964d69
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-06-01 21:10:19 +0100

    body component for search result

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,18 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Search/Header";
+import SearchResults from "../components/Search/SearchResults";
 import res from "../res";
 function Search({ result }) {
+  const router = useRouter();
   console.log(result);
   return (
     <>
       <Head>
-        <title>Search Result</title>
+        <title>{router.query.term} - Google Search</title>
       </Head>
       <Header />
+      <SearchResults result={result} />
     </>
   );
 }
@@ -19,11 +23,12 @@ export async function getServerSideProps(context) {
   const useDummyData = true;
   const apiKey = process.env.CUSTOM_SEARCH_API;
   const contextKey = process.env.CONTEXT_KEY;
-  console.log(apiKey, contextKey);
+  const startIndex = context.query.start || "0";
+
   const data = useDummyData
     ? res
     : await fetch(
-        `https://customsearch.googleapis.com/customsearch/v1?key=${apiKey}&cx=${contextKey}&q=${context.query.term}`
+        `https://customsearch.googleapis.com/customsearch/v1?key=${apiKey}&cx=${contextKey}&q=${context.query.term}&start=${startIndex}`
       ).then((res) => res.json());
   return {
     props: {
